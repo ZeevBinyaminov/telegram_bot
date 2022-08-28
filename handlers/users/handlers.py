344fe1b5186736875cd280badc4f1bc152a03913
ZeevBinyaminov <zzbinyaminov@outlook.com>
2022-08-28 16:56:27 +0300

    done

--- handlers/users/handlers.py
+++ handlers/users/handlers.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 import logging
 from datetime import datetime, timedelta
-from config import ADMIN_ID, OPTIMUM_CHAT_ID
+from config import OPTIMUM_CHAT_ID
 
 from loader import dp, bot
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
@@ -38,10 +38,14 @@ async def welcome(message: Message):
         users_dict = json.load(users_file)
         user_id = str(message.from_user.id)
         username = message.from_user.username
+        first_name = message.from_user.first_name
+        last_name = message.from_user.last_name
 
     if user_id not in users_dict:
         users_dict[user_id] = {
-            "username": username
+            "username": username,
+            "first_name": first_name,
+            "last_name": last_name,
         }
 
     with open("users.json", "w") as users_file:
@@ -89,7 +93,7 @@ async def choose_events(call: CallbackQuery):
     logging.info(f"call = {callback_data}")
     if events_menu == InlineKeyboardMarkup():
         await call.message.edit_text(text="На ближайшее вреня нет событий")
-        await call.message.edit_reply_markup()
+        # await call.message.edit_reply_markup()
     else:
         await call.message.edit_text(text="Список ближайших событий")
         await call.message.edit_reply_markup(reply_markup=events_menu)
@@ -98,7 +102,7 @@ async def choose_events(call: CallbackQuery):
 @dp.message_handler(commands=['events'])
 async def get_events(message: Message):
     if events_menu == InlineKeyboardMarkup():
-        await message.answer(text="На ближайшее вреня нет событий")
+        await message.answer(text="На ближайшее время нет событий")
     else:
         await message.answer(text='Список ближайших событий', reply_markup=events_menu)
 
@@ -143,13 +147,16 @@ async def choose_event(call: CallbackQuery, callback_data: dict):
 
 
 # adding new subject
-@dp.message_handler(commands=["add_subject"], user_id=ADMIN_ID, state=None)
+@dp.message_handler(commands=["add_subject"], state=None)
 async def add_subject(message: Message):
-    await Subject.subject_name.set()
-    await message.answer("Введи название предмета")
+    if await is_admin(message):
+        await Subject.subject_name.set()
+        await message.answer("Введи название предмета")
+    else:
+        await message.answer(text="Вы не являетесь админом")
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Subject.subject_name)
+@dp.message_handler(state=Subject.subject_name)
 async def add_subject_name(message: Message, state: FSMContext):
     if message.text in subjects_dict:
         await message.answer(text="Такой предмет уже есть!")
@@ -162,7 +169,7 @@ async def add_subject_name(message: Message, state: FSMContext):
         await message.answer("Теперь введи ссылку на Instagram-плейлист")
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Subject.instagram_link)
+@dp.message_handler(state=Subject.instagram_link)
 async def add_instagram_link(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['instagram_link'] = message.text
@@ -171,7 +178,7 @@ async def add_instagram_link(message: Message, state: FSMContext):
     await message.answer("Теперь введи ссылку на чат")
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Subject.chat_link)
+@dp.message_handler(state=Subject.chat_link)
 async def add_instagram_link(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['chat_link'] = message.text
@@ -180,7 +187,7 @@ async def add_instagram_link(message: Message, state: FSMContext):
     await message.answer("И, наконец, введи ссылку на zoom")
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Subject.zoom_link)
+@dp.message_handler(state=Subject.zoom_link)
 async def add_zoom_link(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['zoom_link'] = message.text
@@ -205,13 +212,16 @@ async def add_subject_json(state: FSMContext):
 # ---
 
 # adding new event
-@dp.message_handler(user_id=ADMIN_ID, commands=['add_event'], state=None)
+@dp.message_handler(commands=['add_event'], state=None)
 async def set_event(message: Message):
-    await Event.event_name.set()
-    await message.answer("Введи название события")
+    if await is_admin(message):
+        await Event.event_name.set()
+        await message.answer("Введи название события")
+    else:
+        await message.answer("Вы не являетесь админом")
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Event.event_name)
+@dp.message_handler(state=Event.event_name)
 async def add_event_name(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['event_name'] = message.text
@@ -220,7 +230,7 @@ async def add_event_name(message: Message, state: FSMContext):
     await message.answer("Теперь введи текст рассылки")
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Event.event_text)
+@dp.message_handler(state=Event.event_text)
 async def add_event_text(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['event_text'] = message.text
@@ -231,7 +241,7 @@ async def add_event_text(message: Message, state: FSMContext):
     )
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Event.event_date)
+@dp.message_handler(state=Event.event_date)
 async def add_event_date(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['event_date'] = message.text
@@ -242,7 +252,7 @@ async def add_event_date(message: Message, state: FSMContext):
     )
 
 
-@dp.message_handler(user_id=ADMIN_ID, state=Event.event_time)
+@dp.message_handler(state=Event.event_time)
 async def add_event_time(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['event_time'] = message.text
@@ -251,8 +261,8 @@ async def add_event_time(message: Message, state: FSMContext):
     await state.finish()
     await message.answer(
         f"Событие \"{data['event_name']}\" с текстом:\n \"{data['event_text']}\"\n"
-        f"добавлено и будет разослано {data['event_date']} "
-        f"в {data['event_time']}"
+        f"добавлено и будет разослано за сутки до {data['event_date']}"
+        f" в {data['event_time']}"
     )
 
 
@@ -294,3 +304,10 @@ async def notifier():
                     events_menu = make_events_menu()
 
         await asyncio.sleep(5)
+
+
+async def is_admin(message: Message):
+    user_id = message.from_user.id
+    data = await bot.get_chat_administrators(OPTIMUM_CHAT_ID)
+    admins_id = [admin["user"]["id"] for admin in data]
+    return user_id in admins_id
